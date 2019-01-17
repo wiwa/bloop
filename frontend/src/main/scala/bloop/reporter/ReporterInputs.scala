528e0c2b54bf2b645eed624d55c4774bdb76fd53
Jorge Vicente Cantero <jorgevc@fastmail.es>
2019-01-17 16:57:13 +0100

    sdf

---
+++ frontend/src/main/scala/bloop/reporter/ReporterInputs.scala
@@ -0,0 +1,11 @@
+package bloop.reporter
+
+import bloop.data.Project
+import bloop.logging.Logger
+import bloop.io.AbsolutePath
+
+case class ReporterInputs(
+    project: Project,
+    cwd: AbsolutePath,
+    logger: ObservableLogger
+)
